f3e6a79d55b8a7afe983bc3926819d4adf7a8f34
SebastianZ <sebastianzartner@gmail.com>
2015-10-26 22:00:34 +0100

    Added parsing of translated pages (bug 1217996)

--- svgElementDataScraper.php
+++ svgElementDataScraper.php
@@ -18,18 +18,47 @@
     public $interfaces = [];
 
     public function __construct() {
-    	$this->content = new svgElementContent();
+      $this->content = new svgElementContent();
     }
   }
 
   class svgElementContent {
-  	public $description = '';
+    public $description = '';
   }
 
   $svgData = new svgData();
 
   $locales = [
-      'en-US'
+    'en-US' => [
+      'permittedContent' => 'Permitted content'
+    ],
+    'bn-BD' => [
+      'permittedContent' => 'Permitted content'
+    ],
+    'de' => [
+      'permittedContent' => 'Erlaubter Inhalt'
+    ],
+    'es' => [
+      'permittedContent' => 'Contenido permitido'
+    ],
+    'fr' => [
+      'permittedContent' => 'Contenu autorisé|Contenu authorisé|Contenu permis|Contenu permit|Contenu'
+    ],
+    'ja' => [
+      'permittedContent' => '許可された内容|利用可能な中身'
+    ],
+    'ko' => [
+      'permittedContent' => 'Permitted content'
+    ],
+    'pt-BR' => [
+      'permittedContent' => 'Conteúdo permitido|Contepudo permitido'
+    ],
+    'ru' => [
+      'permittedContent' => 'Permitted content'
+    ],
+    'zh-CN' => [
+      'permittedContent' => '允许的内容物|允许的内容|允许的子元素|允许内容'
+    ]
   ];
   $elementReferenceURL = 'https://developer.mozilla.org/%s/docs/Web/SVG/Element/';
   $outputFolder = 'output';
@@ -37,7 +66,7 @@
   if (!file_exists($outputFolder)) {
     mkdir($outputFolder);
   }
-  foreach ($locales as $locale) {
+  foreach ($locales as $locale => $localeItems) {
     $filePath = $outputFolder . '/elementReference.' . $locale . '.html';
     if (isset($_GET['refresh']) || !file_exists($filePath)) {
       $fetchLocation = sprintf($elementReferenceURL, $locale);
@@ -82,66 +111,72 @@
         }
 
         // Parse categories
-        preg_match('/<table class="standard-table">(?:.+?)<\/table>/s', $response, $infoTableMatches);
-        if (preg_match('/Categories.+?<td>(.+?)<\/td>/s', $infoTableMatches[0], $categoriesMatches)) {
-          $categories = explode(', ', $categoriesMatches[1]);
-          $categories = array_map(function($category) {
-            $words = explode(' ', $category);
-            foreach ($words as $index => $word) {
-              $words[$index] = ($index === 0) ? lcfirst($word) : ucfirst($word);
-            }
-            return implode('', $words);
-          }, $categories);
-          $svgData->elements[$element]->categories = $categories;
-        }
-
-        // Parse permitted content
-        if (preg_match('/Permitted content.+?<td>(.+?)<\/td>/s', $infoTableMatches[0], $contentMatches)) {
-          $svgData->elements[$element]->content->description = [];
-          $svgData->elements[$element]->content->description[$locale] = $contentMatches[1];
-        	if (preg_match('/^(.+?):(<br>\n\s+<a\s+href=".+?">.+?<\/a>.*)+$/s',
-        			$contentMatches[1], $contentElementListMatches)) {
-            $svgData->elements[$element]->content->description[$locale] = $contentElementListMatches[1];
-            preg_match_all('/<a href=".+?">(?:<code>)?(.*?)(?:<\/code>)?<\/a>/', $contentElementListMatches[2], $elementListMatches);
-
-            $elements = array_map(function($element) {
-              if ($element[0] === '<') {
-                return $element;
-              }
-
-              $words = explode(' ', $element);
+        if (preg_match('/<table class="standard-table">(?:.+?)<\/table>/s', $response, $infoTableMatches)) {
+          if (preg_match('/Categories.+?<td>(.+?)<\/td>/s', $infoTableMatches[0], $categoriesMatches)) {
+            $categories = explode(', ', $categoriesMatches[1]);
+            $categories = array_map(function($category) {
+              $words = explode(' ', $category);
               foreach ($words as $index => $word) {
                 $words[$index] = ($index === 0) ? lcfirst($word) : ucfirst($word);
               }
               return implode('', $words);
-            }, $elementListMatches[1]);
-
-            $svgData->elements[$element]->content->elements = $elements;
+            }, $categories);
+            $svgData->elements[$element]->categories = $categories;
           }
-        }
 
-        // Parse attributes
-        if (preg_match('/<h2[^>]*>Attributes<\/h2>.*?(?=<h2)/s', $response, $attributesMatches)) {
-	        preg_match_all('/<a.*?href="(.*?)"/', $attributesMatches[0], $attributeURLs);
+          // Parse permitted content
+          if (preg_match('/(?:' . $locales['en-US']['permittedContent'] . '|' .
+              $localeItems['permittedContent'] . ').+?<td>(.+?)<\/td>/su', $infoTableMatches[0], $contentMatches)) {
+            if (!isset($svgData->elements[$element]->content->description)) {
+              $svgData->elements[$element]->content->description = [];
+            }
+            $svgData->elements[$element]->content->description[$locale] = $contentMatches[1];
+            if (preg_match('/^(.+?):?(<br\/?>\n\s+<a\s+href=".+?">.+?<\/a>.*)+$/su',
+                $contentMatches[1], $contentElementListMatches)) {
+              $svgData->elements[$element]->content->description[$locale] = $contentElementListMatches[1];
+              if ($locale === 'en-US') {
+                preg_match_all('/<a href=".+?">(?:<code>)?(.*?)(?:<\/code>)?<\/a>/', $contentElementListMatches[2], $elementListMatches);
 
-	        foreach ($attributeURLs[1] as $attributeURL) {
-	          $attribute = preg_replace('/^.+\/Attribute/', '', $attributeURL);
-	          if ($attribute[0] === '#') {
-	          	$attributeType = lcfirst(substr($attribute, 1)) . 'Attributes';
-	            array_push($svgData->elements[$element]->attributes, $attributeType);
-	          } else {
-	            array_push($svgData->elements[$element]->attributes, '\'' . substr($attribute, 1) . '\'');
-	          }
-	        }
-        }
+                $elements = array_map(function($element) {
+                  if ($element[0] === '<') {
+                    return $element;
+                  }
 
-        // Parse DOM interfaces
-        if (preg_match('/<h2[^>]*?id="DOM_[iI]nterface"[^>]*>.*?(?=<h2)/s', $response, $domInterfacesMatches)) {
-	        preg_match_all('/<a.*?href="\/.*?\/(?:API|DOM)\/(.+?)"/', $domInterfacesMatches[0], $domInterfacesURLs);
-        	foreach ($domInterfacesURLs[1] as $domInterfacesURL) {
-	          array_push($svgData->elements[$element]->interfaces, $domInterfacesURL);
-	        }
-	        $svgData->elements[$element]->interfaces = array_unique($svgData->elements[$element]->interfaces);
+                  $words = explode(' ', $element);
+                  foreach ($words as $index => $word) {
+                    $words[$index] = ($index === 0) ? lcfirst($word) : ucfirst($word);
+                  }
+                  return implode('', $words);
+                }, $elementListMatches[1]);
+
+                $svgData->elements[$element]->content->elements = $elements;
+              }
+            }
+          }
+
+          // Parse attributes
+          if (preg_match('/<h2[^>]*>Attributes<\/h2>.*?(?=<h2)/s', $response, $attributesMatches)) {
+            preg_match_all('/<a.*?href="(.*?)"/', $attributesMatches[0], $attributeURLs);
+  
+            foreach ($attributeURLs[1] as $attributeURL) {
+              $attribute = preg_replace('/^.+\/Attribute/', '', $attributeURL);
+              if ($attribute[0] === '#') {
+                $attributeType = lcfirst(substr($attribute, 1)) . 'Attributes';
+                array_push($svgData->elements[$element]->attributes, $attributeType);
+              } else {
+                array_push($svgData->elements[$element]->attributes, '\'' . substr($attribute, 1) . '\'');
+              }
+            }
+          }
+
+          // Parse DOM interfaces
+          if (preg_match('/<h2[^>]*?id="DOM_[iI]nterface"[^>]*>.*?(?=<h2)/s', $response, $domInterfacesMatches)) {
+            preg_match_all('/<a.*?href="\/.*?\/(?:API|DOM)\/(.+?)"/', $domInterfacesMatches[0], $domInterfacesURLs);
+            foreach ($domInterfacesURLs[1] as $domInterfacesURL) {
+              array_push($svgData->elements[$element]->interfaces, $domInterfacesURL);
+            }
+            $svgData->elements[$element]->interfaces = array_unique($svgData->elements[$element]->interfaces);
+          }
         }
       }
     }
